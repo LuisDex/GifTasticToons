5db592da97f2b2a86d9a097cf7c32b9380d87f4d
Xerdet <lurobper@gmail.com>
2019-01-06 01:37:35 -0600

    Added some css and cosmetic items

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,19 +1,19 @@
-
+//Starting Array of Cartoon Shows
 var faveToons = ["Danny Phantom", "Invader Zim", "Teen Titans", "Johnny Bravo", "Courage the Cowardly Dog", "Batman Beyond", "Justice League Unlimited", "Gargoyles", "Hey Arnold", "Rockos Modern Life", "Ducktales", "Dexters Lab", "Powerpuff Girls", "Darkwing Duck"];
 
-//Prepares the document before running the JS
+//Prepares the document before running the JS.
 $(document).ready(function () {
 
     // Function that cycles through the faveToons Array to create buttons corresponding to the elements within it. 
     function toonButtons() {
         $("#buttonArea").empty();
         for (var i = 0; i < faveToons.length; i++) {
-
             var toon = $("<button>").addClass("toonBTN btn btn-primary mybtn").attr("toonName", faveToons[i]).text(faveToons[i]);
             $("#buttonArea").append(toon);
         };
     };
 
+    //Function that displays the gifs for the button that was pressed.
     function displayGifs(x) {
         $("#gifArea").empty();
         var toonChoice = x;
@@ -26,6 +26,7 @@ $(document).ready(function () {
         })
 
             .then(function (response) {
+
                 var results = response.data;
                 for (var i = 0; i < results.length; i++) {
                     var toonSpace = $("<div>");
@@ -40,22 +41,26 @@ $(document).ready(function () {
             });
     };
 
+    //Listener that waits for the user to fill in a new toon to add as a button. If the button is pressed, but the text box is empty the function will not run.
     $("#newToon").on("click", function (event) {
         event.preventDefault();
         var newToon = $("#addToon").val().trim();
-        faveToons.push(newToon);
-        toonButtons();
-        $("#addToon").val("");
+        if (newToon != "") {
+            faveToons.push(newToon);
+            toonButtons();
+            $("#addToon").val("");
+        }
     });
 
+//Listener that calls the "Display Gifs" function when a button is pressed.
     $(document).on("click", ".toonBTN", function () {
         displayGifs($(this).attr("toonName"));
     });
 
+//Listener that makes the gifs run or stop when pressed, based on their previous status.
     $(document).on("click", ".toonGIF", function () {
 
         var isMoving = $(this).attr("value");
-        console.log(isMoving);
         if (isMoving === "still") {
             $(this).attr("src", $(this).attr("movingURL"));
             $(this).attr("value", "moving");
@@ -64,6 +69,8 @@ $(document).ready(function () {
             $(this).attr("value", "still");
         }
     });
+
+//Creates the initial buttons from the pre-set array.
     toonButtons();
 
 
